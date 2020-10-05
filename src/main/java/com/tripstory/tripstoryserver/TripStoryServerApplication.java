c6b85475f108764cb338f471db6e580fe88dc505
jeonseun <wjstpdns2015@naver.com>
2020-10-05 21:53:09 +0900

    프로젝트 생성

---
+++ src/main/java/com/tripstory/tripstoryserver/TripStoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.tripstory.tripstoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripStoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripStoryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tripstory/tripstoryserver/TripStoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripstory.tripstoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripStoryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
